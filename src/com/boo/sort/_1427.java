58e4f90194c2986dd492aa91a159809801f1d0ad
b00db <buyeong21@gmail.com>
2020-05-13 03:43:14 +0900

    Selection Sort

---
+++ src/com/boo/sort/_1427.java
@@ -0,0 +1,57 @@
+package com.boo.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _1427 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String N = scanner.nextLine();
+		
+		/* Arrays.sort() 메소드 사용
+		char[] array = new char[N.length()];
+		
+		for(int i=0; i<N.length(); i++) {
+			array[i] = N.charAt(i);
+		}
+		
+		Arrays.sort(array);
+		
+		for(int i=array.length-1; i>=0; i--)
+			System.out.print(array[i]);
+		*/
+		
+		int[] array = new int[N.length()];
+		for(int i=0; i<N.length(); i++) {
+			array[i] = Character.getNumericValue(N.charAt(i));
+		}
+		
+		selectionSort(array);
+		
+		for(int i=0; i<N.length(); i++) {
+			System.out.print(array[i]);
+		}
+	}
+	
+	// 선택 정렬(Selection Sort)
+	static void selectionSort(int A[]) {
+		int size = A.length;
+		int max;
+		int temp;
+		
+		for(int i=0; i<size-1; i++) {
+			max = i;
+			
+			for(int j=i+1; j<size; j++) {
+				if(A[max] < A[j]) {
+					max = j;
+				}
+			}
+			
+			temp = A[max];
+			A[max] = A[i];
+			A[i] = temp;
+		}
+	}
+}
